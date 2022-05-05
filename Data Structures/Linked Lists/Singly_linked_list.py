83ad77311d1392df1de22cd0b57fd16a4398d118
An Khanh Tran <khanhatran2001@gmail.com>
2022-05-04 23:32:27 -0400

    commit

--- Data Structures/Linked Lists/Singly_linked_list.py
+++ Data Structures/Linked Lists/Singly_linked_list.py
@@ -1,13 +1,64 @@
+class node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+
 class Singly_Linked_List:
-    def __init__(self, element, next = None):
-        self.element = element
-        self.next = next
+    def __init__(self, array):
+        root = node(array[0])
+        self.head = root
+        for i in range(1, len(array)):
+            root.next = node(array[i])
+            root = root.next
 
-    def Get(self):
-        pass
+    def GetIndex(self, value):
+        i = 0
+        while self.head is not None:
+            if self.head.value == value:
+                return i
+            i += 1
+        return -1
 
-    def Push(self):
-        pass
+    def Push(self, value):
+        while self.head.next is not None:
+            self.head = self.head.next
+        self.head.next = node(value)
 
-    def Del(self):
-        pass
+    def Push(self, value, index):
+        i = 0
+        while self.head.next is not None:
+            if i == index:
+                break
+            self.head = self.head.next
+
+        prev_node = self.head
+        medium = node(value)
+        con_node = self.head.next
+        prev_node.next = medium
+        medium.next = con_node
+
+    def Pop(self):
+        while self.head.next.next is not None:
+            self.head = self.head.next
+        popped_node = self.head.next
+        self.head.next = popped_node.next
+        temp = popped_node.value
+        del popped_node
+        return temp
+
+    def Pop(self,index):
+        i = 0
+        while self.head.next.next is not None:
+            if i == index:
+                break
+            self.head = self.head.next
+            i += 1
+        popped_node = self.head.next
+        self.head.next = popped_node.next
+        temp = popped_node.value
+        del popped_node
+        return temp
+
+
+
--- Data Structures/Linked Lists/doubly_linked_list.py
+++ Data Structures/Linked Lists/doubly_linked_list.py
@@ -1,15 +1,29 @@
+class node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        self.before = None
+
 class Singly_Linked_List:
-    def __init__(self, element, next = None, before = None):
+    def __init__(self, element = None, aList = None, next = None, before = None):
         self.element = element
         self.next = next
         self.before = before
+        self.aList = aList
 
-    def Get(self):
+    def GetIndex(self,value):
         pass
 
-    def Push(self):
+    def Push(self,value):
         pass
 
-    def Del(self):
+    def Push(self,value,index):
         pass
+
+    def Pop(self):
+        pass
+
+    def Pop(self,index):
+        pass
+
 
