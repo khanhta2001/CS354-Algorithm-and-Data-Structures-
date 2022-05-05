0438c8df59970cc84a458bf49c21586d5a26551c
An Khanh Tran <khanhatran2001@gmail.com>
2022-05-04 23:34:52 -0400

    commit

--- Data Structures/Tree/Tree.py
+++ Data Structures/Tree/Tree.py
@@ -1,1 +1,16 @@
+class node:
+    def __init__(self, value):
+        self.value = value
+        self.children = []
+
+class Tree:
+    def __init__(self,dictionary=None):
+        self.dictionary = dictionary
+
+
+    def push(self, value):
+        self.children.append(value)
+
+    def pop(self, value):
+        self.children.remove(value)
 
