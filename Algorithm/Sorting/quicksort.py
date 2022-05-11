fa4cfa8678802daa29c905a575c3db2ed4dcd0ac
An Khanh Tran <khanhatran2001@gmail.com>
2022-05-10 20:05:59 -0400

    commit

--- Algorithm/Sorting/quicksort.py
+++ Algorithm/Sorting/quicksort.py
@@ -1,40 +1,3 @@
-def isSorted(array):
-    # check if the list is sorted or not
-    for i in range(len(array)-1):
-        if array[i] > array[i+1]:
-            return False
-    return True
-
-def Sort(array):
-    # sort the list using quicksort
-    quicksort(array, 0, len(array)-1)
-
-
-def BinarySearch(array, target):
-    # run binary search
-    low = 0
-    high = len(array)
-    while low <= high:
-        mid = low + (high-low)//2
-
-        # check if the middle element equals, smaller, or larger than the target
-        if array[mid] == target:
-            return mid
-        elif array[mid] > target:
-            high = mid - 1
-        else:
-            low = mid + 1
-    #if not found, return -1
-    return -1
-
-def BinarySearch_Helper(array, target):
-    if isSorted(array):
-        return BinarySearch(array, target)
-    Sort(array)
-    return BinarySearch(array, target)
-
-
-
 def partition(array, low, high):
     pivot = array[high]
     pos = low - 1
@@ -66,4 +29,3 @@ def quicksort(array, low, high):
 
         # recursive call on the right of the pivot
         quicksort(array, pivot + 1, high)
-
