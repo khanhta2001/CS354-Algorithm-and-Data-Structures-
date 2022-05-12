148e01faebeb0c2f8f2c9e62edea029d388fe43a
An Khanh Tran <khanhatran2001@gmail.com>
2022-05-12 11:20:01 -0500

    commit

--- Data Structures/Queues/queue.py
+++ Data Structures/Queues/queue.py
@@ -1,1 +1,20 @@
+class Queue:
+    def __init__(self, array=None):
+        if array is None:
+            self.array = []
+        else:
+            self.array = array
 
+    def enqueue(self, element):
+        self.array.append(element)
+
+    def dequeue(self):
+        first_element = self.array[0]
+        self.array.remove(first_element)
+        return first_element
+
+    def size(self):
+        return len(self.array)
+
+    def peek(self, index):
+        return self.array[index]
--- Data Structures/Stacks/stack.py
+++ Data Structures/Stacks/stack.py
@@ -1,1 +1,20 @@
+class Stack:
+    def __init__(self,array=None):
+        if array is None:
+            self.array = []
+        else:
+            self.array = array
 
+    def push(self, element):
+        self.array.append(element)
+
+    def Pop(self):
+        element = self.array[-1]
+        self.array.remove(element)
+        return element
+
+    def size(self):
+        return len(self.array)
+
+    def peek(self, index):
+        return self.array[index]
