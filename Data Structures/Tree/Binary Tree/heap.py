b9366a86076be3fac483862f5349dee2e7bfc58f
An Khanh Tran <khanhatran2001@gmail.com>
2022-05-14 17:41:22 -0500

    commit

--- Data Structures/Tree/Binary Tree/heap.py
+++ Data Structures/Tree/Binary Tree/heap.py
@@ -7,5 +7,15 @@ class Node:
 class HeapTree:
     def __init__(self):
         pass
+
     def HeapSort(self):
+        pass
+
+    def get(self):
+        pass
+
+    def push(self):
+        pass
+
+    def pop(self):
         pass
