4114a1ec32f06b5e42255aa400e31ba6f89d48af
An Khanh Tran <khanhatran2001@gmail.com>
2022-05-18 21:34:55 -0500

    commit

---
+++ Algorithm/Greedy Methods/graph.py
@@ -0,0 +1,33 @@
+class Graph:
+    def __init__(self, data):
+        self.a = 1
+
+    def adjacent(self, x, y):
+        pass
+
+    def neighbors(self,x):
+        pass
+
+    def add_vertex(self, x):
+        pass
+
+    def remove_vertex(self,x):
+        pass
+
+    def add_edge(self, x, y):
+        pass
+
+    def remove_edge(self, x,y):
+        pass
+
+    def get_vertex_value(self, x):
+        pass
+
+    def set_vertex_value(self, x, value):
+        pass
+
+    def get_edge_value(self,x,y):
+        pass
+
+    def set_edge_value(self,x,y,v):
+        pass 
